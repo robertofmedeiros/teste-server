df2b307d66e1e297a955b8e3c591a424a83b6d32
robertofmedeiros <roberto_p.v@outlook.com>
2021-04-15 13:34:13 -0300

    - primeiro commit

---
+++ src/main/java/com/example/teste/TesteApplication.java
@@ -0,0 +1,22 @@
+package com.example.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Teste para o jenkins");
+		System.out.println("Testando logs");
+		System.out.println("Pausando");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Continuando");
+		System.out.println("Fim");
+	}
+
+}
